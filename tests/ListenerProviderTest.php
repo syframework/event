eaa2f803682b1d047091a510bf7c109dc3754a30
syone <syone7@gmail.com>
2021-09-25 12:06:36 +0200

    add some tests

--- tests/ListenerProviderTest.php
+++ tests/ListenerProviderTest.php
@@ -13,15 +13,35 @@ class ListenerProviderTest extends \PHPUnit\Framework\TestCase {
 
 	public function testAddListener() {
 		$event = new Event('foo');
-		$this->assertEmpty($this->listenerProvider->getListenersForEvent($event));
+
+		// Check if it is an empty iterable at first
+		$listeners = $this->listenerProvider->getListenersForEvent($event);
+		$this->assertIsIterable($listeners);
+		if (is_array($listeners)) {
+			$this->assertEmpty($listeners);
+		} else {
+			$this->assertEquals(0, iterator_count($listeners));
+		}
+
+		// Add a listener
 		$listener = function ($event) {
 			echo $event->getName();
 		};
 		$this->listenerProvider->addListener('foo', $listener);
+
+		// Check if it is not an empty iterable now
 		$listeners = $this->listenerProvider->getListenersForEvent($event);
-		$this->assertIsArray($listeners);
-		$this->assertIsCallable($listeners[0]);
-		$this->assertSame($listener, $listeners[0]);
+		$this->assertIsIterable($listeners);
+		if (is_array($listeners)) {
+			$this->assertNotEmpty($listeners);
+		} else {
+			$this->assertGreaterThan(0, iterator_count($listeners));
+		}
+
+		// Check if listener is a callable
+		$l = is_array($listeners) ? current($listeners) : $listeners->current();
+		$this->assertIsCallable($l);
+		$this->assertSame($listener, $l);
 	}
 
 }
